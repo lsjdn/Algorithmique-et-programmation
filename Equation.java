a6357c35bb0b18b06769c2e1a6d03ec1c419f069
Louis Jourdan <ljourdan@pc-l115-6.isep.fr>
2016-02-01 12:28:19 +0100

    First commit

---
+++ Equation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Equation {
+	public static void main ( String [] args) {
+		Scanner scan = new Scanner(System.in);		
+		double a= scan.nextDouble();
+		double b= scan.nextDouble();
+		double c= scan.nextDouble();
+		double D= b*b + 4*a*c ;
+		if (D>0);{
+			}
+
+
+	}
+}
